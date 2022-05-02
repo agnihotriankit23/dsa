be1d6ef8ce0fb170a2bd822ec44fae2ac71aff02
Ankit agnihotri <agnihotriankit54@gmail.com>
2022-05-02 11:24:13 +0530

    add gitignore

--- arrays/binary_search.cpp
+++ arrays/binary_search.cpp
@@ -1,10 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
+void print(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+
+    }
+    cout<<endl;
+}
 
 int binarySearch(int arr[],int n,int key){
     int s=0,e=n-1;
     while(s<=e){
-        int mid=(s+e)/2;
+        int mid=s +(e-s)/2 ;
         if(arr[mid]==key){
             return mid;
         }else if(arr[mid]>key){
@@ -15,15 +22,18 @@ int binarySearch(int arr[],int n,int key){
 }
 
 int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+    
+    int arr[6]={2,4,6,8,12};
+    int odd[5]={1,3,8,11,14};   
 
-    int key;
-    cin>>key; 
-    cout<<binarySearch(arr,n,key);
+    int index=binarySearch(arr,6,12);
+    cout<<"Element 12 is at postion "<<index<<endl;
+     index=binarySearch(odd,5,3);
+    cout<<"Element 3 is at postion "<<index<<endl;
+    
+
+    // int key;
+    // cin>>key; 
+    // cout<<binarySearch(arr,n,key);
     return 0;
 }
